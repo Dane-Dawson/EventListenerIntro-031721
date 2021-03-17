2a80065faedb28c5e4e1b8cd5e76198eba4979b6
Your Name <you@example.com>
2021-03-17 13:02:57 -0600

    updated

---
+++ README.md
@@ -0,0 +1,5 @@
+#Introduction to Events with JS
+
+A practice in JavaScript event listeners! 
+
+Recorded on Marc 17, 2021 with the Denver campus of Flatiron.
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const topElement = document.getElementById("top")
 const middleElement = document.getElementById("middle")
 const bottomElement = document.getElementById("bottom")
-
-bottomElement.addEventListener("click", (e)=>{
-    console.log("click")
-})
