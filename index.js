15815bed3258c7251525ffee4673e2f5ac09f192
Your Name <you@example.com>
2021-03-17 12:49:21 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const topElement = document.getElementById("top")
+const middleElement = document.getElementById("middle")
+const bottomElement = document.getElementById("bottom")
+
+bottomElement.addEventListener("click", (e)=>{
+    console.log("click")
+})
