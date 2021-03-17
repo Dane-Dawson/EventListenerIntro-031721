d0adf318cda05773ff9aa18c510c34573bf53185
Your Name <you@example.com>
2021-03-17 15:22:29 -0600

    Tanner says so

--- index.js
+++ index.js
@@ -33,12 +33,29 @@ formElement.addEventListener('submit', (event)=>{
     createWelcomeBanner(event.target.firstname.value, event.target.lastname.value)
 })
 
-function createWelcomeBanner(firstName, lastName){
+function createWelcomeBanner(gesundheit, lastName){
     let newName = document.createElement('h3')
-    newName.innerText = `Welcome to our club ${firstName} ${lastName}!`
+    newName.innerText = `Welcome to our club ${gesundheit} ${lastName}!`
     bottomElement.append(newName)
 }
 
 // let funArray = [1, 2, 3, 4, 5]
 // funArray.forEach(number=>bottomElementResponse())
 // bottomElement.addEventListener(eventItsListeningFor, functionInvokedWhenEventhappens)
+
+
+function doAThing(){
+    console.log("Hurrah")
+}
+//I am writing this function to take in an array of numbers and total them
+//[3, 13, 47, 3]
+function totalSum(numberArray){
+    let total = 0
+    numberArray.forEach(number=> total += number)
+    return total
+}
+
+//Expecting a string that is a name
+function sayHiByName(materhorn){
+    return `Hello ${materhorn}`
+}
